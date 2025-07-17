4e7adbd49de28ab0212419a12b2365bb4b5379c6
bachaudhry <b.a.chaudhry@unswalumni.com>
2025-07-17 17:26:07 +0500

    Update training loop with new components and complete training run

--- supplementary/utils/components.py
+++ supplementary/utils/components.py
@@ -1,6 +1,8 @@
 import tiktoken
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -245,3 +247,96 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         else: 
             break
     return total_loss / num_batches
+
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, 
+                       eval_freq, eval_iter, start_context, tokenizer):
+    # Initialize lists to track losses and tokens seen
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    
+    # Main training loop
+    for epoch in range(num_epochs):
+        model.train()
+        
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad() # reset loss gradients from previous batch iteration
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+            
+            # Additional eval step
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Epoch {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+                
+        # Print sample text after each epoch
+        generate_and_print_sample(
+            model, tokenizer, device, start_context)
+        
+    return train_losses, val_losses, track_tokens_seen
+    
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    # Convenience function to generate text at regular training intervals to
+    # monitor performance.
+        model.eval()
+        context_size = model.pos_emb.weight.shape[0]
+        encoded = text_to_token_ids(start_context, tokenizer).to(device)
+        with torch.no_grad():
+            token_ids = generate_text_simple(
+                model=model, idx=encoded,
+                max_new_tokens=50, context_size=context_size
+                )
+        decoded_text = token_ids_to_text(token_ids, tokenizer)
+        print(decoded_text.replace("\n", " ")) # Compact print format
+        model.train()        
+        
+        
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # Plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))  # only show integer labels on x-axis
+
+    # Create a second x-axis for tokens seen
+    ax2 = ax1.twiny()  # Create a second x-axis that shares the same y-axis
+    ax2.plot(tokens_seen, train_losses, alpha=0)  # Invisible plot for aligning ticks
+    ax2.set_xlabel("Tokens seen")
+
+    fig.tight_layout()  # Adjust layout to make room
+    #plt.savefig("loss-plot.pdf")
+    plt.show()
+    
+    
+# Pipeline functions
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # Add batch dimension
+    return encoded_tensor
+
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0) # Removing batch dim
+    return tokenizer.decode(flat.tolist())
