135a438218331fced0d7880f0ed2044237f5ed71
bachaudhry <b.a.chaudhry@unswalumni.com>
2025-07-17 16:40:59 +0500

    Gradient clipping + util funcs + updated gitignore

--- supplementary/utils/components.py
+++ supplementary/utils/components.py
@@ -217,3 +217,31 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
 
     return idx
+
+
+# Function to calculate the loss of a single batch
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss
+
+
+# Calculate the loss over all batches sampled by a given data loader.
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce num of batches to match the total number of batches in the data loader
+        # in case values are exceeded.
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else: 
+            break
+    return total_loss / num_batches
