e6f757ab6d45fcef9ef01c05faa25d8fee25fce9
bachaudhry <b.a.chaudhry@unswalumni.com>
2025-10-02 15:12:04 +0500

    Setup components.py - dataloader and MHA

---
+++ supplementary/experiments-classification-finetuning-imdb/utils/components.py
@@ -0,0 +1,51 @@
+# Source for "Build a Large Language Model From Scratch"
+#   - https://www.manning.com/books/build-a-large-language-model-from-scratch
+# Code: https://github.com/rasbt/LLMs-from-scratch
+
+
+import numpy as np
+import tiktoken
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+
+### Dataset and Dataloader ###
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.tokenizer = tokenizer
+        self.input_ids = []
+        self.target_ids = []
+        
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+        
+        # Sliding window to chunk the text into overlapping sequences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+            
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+    
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128,
+                         shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers
+    )
+    return dataloader
+
+
+### Attention Mechanism ###
+
+class MultiHeadAttention(nn.Module):
+    
